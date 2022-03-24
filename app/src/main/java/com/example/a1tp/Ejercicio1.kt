b66c9a3a8468ee3b7b7f3349ff539e9e79a5e5fe
fierro223 <emilianofierro@hotmail.com.ar>
2022-03-24 09:55:48 -0300

    first commit

---
+++ app/src/main/java/com/example/a1tp/Ejercicio1.kt
@@ -0,0 +1,8 @@
+fun main(){
+    val numero1:Int=2
+    val numero2:Int=5
+    val numero3:Int=6
+    val total:Int=(numero1+numero2+numero3)/3
+    println("El Promedio de los 3 numeros es $total")
+}
+
---
+++ app/src/main/java/com/example/a1tp/Ejercicio2.kt
@@ -0,0 +1,16 @@
+fun main(){
+    val numero1:Double=4.00
+    val numero2:Double=7.00
+    val suma:Double= numero1+numero2
+    val resta:Double= numero1-numero2
+    val producto:Double=numero1*numero2
+    val division: Double=numero1/numero2
+    if (numero1<numero2){
+        println("la Suma es:$suma, la Resta es:$resta ")
+
+    }else{
+        println("El Producto es:$producto, la Division es:$division ")
+
+    }
+}
+
